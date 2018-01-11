84df42adab324c212187a961c4c1c27e3d48caa4
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-11 16:37:15 +0530

    Rename ten.c to tens.c

---
+++ tens.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#inlude<conio.h>
+void main()
+{
+long long n;
+int count=0;
+printf("enter the number");
+scanf("%11d,n);
+while(n!=0)
+{
+n/=10;
+++count;
+}
+printf("numberis:%d",count
+}
