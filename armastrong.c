c4d65c06301a9f26d227781b91322b8be6e3ad0c
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-19 16:51:11 +0530

    Create armastrong.c

---
+++ armastrong.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int sum=0,d,r,n=153;
+t=n;
+while(t!=0)
+{
+r=n%10;
+d=r*r*r;
+sum=sum+d;
+t=n/10;
+}
+if(sum==n)
+{
+printf("armstrong",n);
+}
+else
+{
+printf("not amstrong");
+}
+}
