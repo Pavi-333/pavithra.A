631ac006a3456bb3f6b3a207967df8d44e2a3bf7
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-19 16:03:27 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+inta=2,b=6,m=100;
+if(a>b&&a>c)
+{
+printf("a is large);
+}
+else if(b>c)
+{
+printf("b is large);
+}
+else
+{
+printf("c is large);
+}
+}
