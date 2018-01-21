3fa170c57fe8abe73ee9d149b56b986b54972000
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-21 09:30:14 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i
+for(i=0;i<=10:i++)
+{
+for(i=2;i<=n%2;++i)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+if(flag==0)
+{
+printf("prime",i);
+}
+else
+{
+printf("not prime",i);
+}
