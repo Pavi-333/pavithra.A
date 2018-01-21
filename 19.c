9ee6780a7d2487cf163b04430e917352a80e19b8
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-21 09:47:32 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,fact=1;
+scanf("%d",&n)
+printf("the num is",n)
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+printf("the factorial is",fact)
+}
