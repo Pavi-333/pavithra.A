3db80734b9d5caf4b6964bfa129d9850bd761104
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-21 11:46:57 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[29];
+int flag=0,i;
+printf("enter the string");
+scanf("%s",s);
+for(i=0;s[i]='\0';i++)
+{
+if(s[i]='0'||s[i]='1'||s[i]='2'||s[i]='3'||s[i]='4'||s[i]='5'||s[i]='6'||s[i]='7'||s[i]='8'||s[i]='9')
+{
+flag=flag+1;
+}
+}
+if(flag==0)
+{
+printf("the num is string")
+}
+else 
+{
+printf("not srting")
+}
