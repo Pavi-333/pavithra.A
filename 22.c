22b33aa8ea7b951fd113fc28519e016588a31981
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-21 10:15:19 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,temp,a[10],j;
+printf("the num is",n);
+scanf("%d",n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n:j++)
+{
+if(a[i]>a[j]
+{
+temp=a[i];
+}
+else
+{
+temp=a[j];
+}
+printf(" themaximum is",temp);
+}
