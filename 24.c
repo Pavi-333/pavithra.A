16cb3eb2ad4e2bc1771e7887a6ac958899eac0cf
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-21 11:07:23 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10];
+int size,i,j,temp;
+printf("enter the size");
+scanf("%d",size);
+printf("enter the array",);
+for(i=0;i<=size;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=size;i++)
+{
+for(j=i+1;j<=size;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<=size;i++)
+{
+scanf("%d",a[i]);
+}
+}
