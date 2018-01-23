afd5d3acb4f473e709f9e2567ea1186c7356fb2d
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-01-23 16:25:00 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h1,h2,mi,m2,mf,hf;
+printf("enterh1&m1");
+scanf("%d%d",h1,m1);
+printf("enter h2&m2");
+scanf("%d%d",h2,m2);
+hf=h1-h2;
+mf=m1-m2;
+printf("%d:%d",hf,mf);
+getch();
+}
