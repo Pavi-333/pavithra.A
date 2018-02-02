c9763f02ef7628b99abf89135130374a6267d642
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-02-02 09:34:29 +0000

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char s[100];
+int i,count=0;
+printf("enter the string");
+scanf("%[^/s]",&s);
+for(i=o;s[i]!='\0';i++0
+{
+if(s[i]==' ')
+{
+count++;
+}
+printf("%d",count);
+}
