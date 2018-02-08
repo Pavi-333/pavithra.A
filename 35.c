0048050112d587191ecc015fca160406e450aaa0
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-02-08 09:13:21 +0000

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+char s[10];
+int i,count=0;
+printf("enter the string");
+scanf("%s",s);
+if(s[i] >= 'A' && <= 'Z')
+{
+break;
+}
+if(s[i] >= 'a' && <= 'z')
+{
+break;
+}
+if(s[i] >= '0' && <= '9')
+{
+count++
+}
+printf("%d",count);
+}
+
+
