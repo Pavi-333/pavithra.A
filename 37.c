966681e00fe90224a271db540e8fae6999c8d48f
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-02-08 09:31:47 +0000

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int p,m,temp;
+printf("Enter the two numbers:");
+scanf("%d%d",&a,&b);
+printf("Before swapping a=%d b=%d",a,b);
+temp=p;
+p=m;
+m=temp;
+printf("\n After swapping %d %d",a,b);
+return 0;
+}
