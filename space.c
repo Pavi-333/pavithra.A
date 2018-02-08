813ff70270a5d764239d56d408033ffa05bc472f
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-02-08 08:47:02 +0000

    Rename 33.c to space.c

---
+++ space.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char s[10],
+int count=0,i;
+printf("enter the line"):
+scanf("%s",s):
+for(i=0;s[i]='\0';i++)
+{
+if(s[i]==' ')
+{
+count++;
+}
+printf("%d",count);
+}
