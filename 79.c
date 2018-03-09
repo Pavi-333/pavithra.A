589e787b9ca02931211f93b00c6667d839683eac
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-03-09 15:59:45 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c,s;
+  printf("enter the numbers");
+  scanf("%d%d",&a,&b);
+  c=a*b;
+  s=a*a;
+  if(c==s)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+}
