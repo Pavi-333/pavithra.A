cc17ea3ba3c75170d2bb6eac697c6906e9d16a17
Pavi-333 <30828704+Pavi-333@users.noreply.github.com>
2018-03-20 15:56:11 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the  number");
+    scanf("%d",&a);
+    while(a!=0)
+    {
+        b=a%10;
+        if(b%2==1)
+        {
+        printf("%d\n",b);
+        }
+        a=a/10;
+    }
+    return 0;
+}
